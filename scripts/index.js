725911e591bf760cd594fb67c38c8df52ed31bbb
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-09-01 20:10:44 +0200

    add cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 /**
  * @param[fetchData]
- * 
+ *
  */
 const fetchData = async () => {
   try {
@@ -9,7 +9,7 @@ const fetchData = async () => {
     });
     if (requete.ok) {
       const data = await requete.json();
-           return data;
+      return data;
     }
   } catch (e) {
     console.log(e);
@@ -21,15 +21,14 @@ const fetchData = async () => {
  * @param[reverse dataArray]
  */
 async function processRecipes() {
-    const dataArray = await fetchData();  
-    dataArray.reverse();
-    dataArray.forEach((data) => {
-      card(data); 
-    });
-  }
-  
-  processRecipes();
+  const dataArray = await fetchData();
+  dataArray.reverse();
+  dataArray.forEach((data) => {
+    card(data);
+  });
+}
 
+processRecipes();
 
 function card(data) {
   const { name, description, quantity, unit, ingredients, image, time } = data;
@@ -37,34 +36,42 @@ function card(data) {
 
   //
 
-
- // Créer une liste d'ingrédients en parcourant le tableau d'ingrédients
- const ingredientsList = ingredients.map((ingr) => `
+  // Créer une liste d'ingrédients en parcourant le tableau d'ingrédients
+  const ingredientsList = ingredients
+    .map(
+      (ingr) => `
  <li>
    <p>${ingr.ingredient}</p>
-   ${ingr.quantity ? `<p>${ingr.quantity} ${ingr.unit || ''}</p>` : ''}
+   ${ingr.quantity ? `<p class="text-slate-400 	tracking-widest	">${ingr.quantity} ${ingr.unit || ""}</p>` : ""}
  </li>
-`).join('');
+`
+    )
+    .join("");
+    const maxLength = 180; // Le nombre maximal de caractères que vous souhaitez afficher
 
+    // Tronquer la description si elle dépasse maxLength
+    const truncatedDescription = description.length > maxLength
+      ? `${description.substring(0, maxLength)}...` // Tronquer et ajouter des points de suspension
+      : description;
   //
   const cadre = `
-    <figure class="cadre ">  
-    <div>
-    <img   src="${picture}" alt="recette de ${name}">
-    <p> ${time} </p>
+    <figure class="cadre  w-[380px] h-[731] bg-white rounded-3xl">  
+    <div class="h-[253px] w-full rounded-3xl">
+    <img  class="h-full w-full object-cover rounded-3xl rounded-b-lg" src="${picture}" alt="recette de ${name}">
+    <p class="relative w-[80px] bg-yellow-500  text-center h-7 rounded-3xl 	top-[-232px]  left-[291px]"> ${time} min </p>
     </div>
-    
-    <h1> ${name} </h1>
-    <p>RECETTE</p>
-    <figcaption> ${description} </figcaption>
+    <div class="flex flex-col py-7 w-4/5 mx-auto gap-4">
+    <h1 class="font-anton text-lg font-normal "> ${name} </h1>
+    <p class="text-slate-400 font-bold	tracking-widest	">RECETTE</p>
+    <figcaption> ${truncatedDescription} </figcaption>
     <div class="">
-    <p> Ingrédients </p>
+    <p class="text-slate-400 font-bold	tracking-widest	uppercase pb-3"> Ingrédients </p>
 
-    <ul>
+    <ul class="grid  grid-cols-2 mx-auto gap-7">
       ${ingredientsList}
     </ul>
   </div>
-  
+  </div>
     </figure>
     `;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     extend: {
       colors: {
         primary: "#FFD15B",
+        second:"#7A7A7A",
       },
 
       fontFamily: {
         anton: ["Anton", "sans-serif"],
+        Manrope: ["Manrope"],
       },
       backgroundImage: {
         header: "url('./assets/images/header.jpg')",
