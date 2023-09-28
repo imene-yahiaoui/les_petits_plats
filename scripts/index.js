eff8e52d3dc9c5cfa146e1ca0ff52872967e9302
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-09-28 15:50:11 +0200

    Add documentation for Search With Input

--- scripts/index.js
+++ scripts/index.js
@@ -34,22 +34,25 @@ function updateNumberOfCards() {
   );
 }
 
-//////////////////recherche////////////
-searchValue.addEventListener("input", SeracheWithInput);
-
-function SeracheWithInput() {
+function SerachWithInput() {
   const valeurDeRecherche = searchValue.value.toLowerCase();
   performSearch(valeurDeRecherche);
 }
-
+/**
+ * @param[valeurDeRecherche]
+ * @return[matchCadres]
+ */
 function performSearch(valeurDeRecherche) {
   const existingNoMatchMessage = document.getElementById("NoMatchview");
+  const existingTag = document.getElementById("tags");
   const cadresRecettes = pageObject.visibleCadres();
   const matchCadres = [];
+  // Supprimer un message existant s'il y en a
   if (existingNoMatchMessage) {
     existingNoMatchMessage.remove();
   }
-console.log('tagSection.length',tagSection.length)
+  console.log("tagSection.length", tagSection.length);
+  // Parcourir les cadres de recettes visibles
   cadresRecettes.forEach((cadre) => {
     const titre = cadre.querySelector(".titlesCadre").textContent.toLowerCase();
 
@@ -59,6 +62,7 @@ console.log('tagSection.length',tagSection.length)
     const ingredients = cadre
       .querySelector(".ingredientsCard")
       .textContent.toLowerCase();
+    // Vérifier si la valeur de recherche est présente dans le titre ou la description ou les ingrédients
 
     if (
       titre.includes(valeurDeRecherche) ||
@@ -68,22 +72,29 @@ console.log('tagSection.length',tagSection.length)
       matchCadres.push(cadre);
     }
   });
+  // Effacer le contenu actuel de l'affichage des cadres
   pageObject.cadre().innerHTML = "";
+
+  // Filtrer les cadres en fonction de la valeur de recherche si elle est plus de 2  caractères
   if (valeurDeRecherche.length > 2) {
     matchCadres.forEach((cadre) => {
       pageObject.cadre().appendChild(cadre);
     });
-  } else if (valeurDeRecherche.length <= 2 ) {
-    
+  }
+  // Gérer le cas où la valeur de recherche est courte (<= 2 caractères) il retourne la liste original
+  if (valeurDeRecherche.length <= 2) {
+    if (existingTag) {
+      elementValues = [];
+    }
     originalCadres.forEach((cadre) => {
       pageObject.DisplayCard(cadre);
     });
   }
-
+  // Gérer le cas où aucun résultat n'est trouvé
   if (valeurDeRecherche.length > 2 && matchCadres.length === 0) {
     console.log("le resu est 0", valeurDeRecherche.length);
     searchValue.value = "";
-     main.insertAdjacentHTML("afterend", NoMatchCard(valeurDeRecherche));
+    main.insertAdjacentHTML("afterend", NoMatchCard(valeurDeRecherche));
   }
 
   updateNumberOfCards();
@@ -91,6 +102,8 @@ console.log('tagSection.length',tagSection.length)
   updateAppareilList();
   updateUstensileList();
 }
+
+searchValue.addEventListener("input", SerachWithInput);
 
 BtnSearche.addEventListener("click", (e) => {
   e.preventDefault();
